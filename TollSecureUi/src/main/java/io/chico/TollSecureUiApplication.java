bb5e3ac95ba4de686f326fbbedd7fd69f48a826d
Chico Almeida <francisco.almeida@outlook.com>
2016-11-21 23:16:22 -0200

    creating basic authorization client using github auth

---
+++ TollSecureUi/src/main/java/io/chico/TollSecureUiApplication.java
@@ -0,0 +1,12 @@
+package io.chico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollSecureUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollSecureUiApplication.class, args);
+	}
+}
---
+++ TollSecureUi/src/main/java/io/chico/controller/ReportController.java
@@ -0,0 +1,28 @@
+package io.chico.controller;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableOAuth2Sso
+public class ReportController extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping("/")
+    public String loadHome() {
+        return "home";
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                  .antMatchers("/", "/login**")
+                  .permitAll()
+                .anyRequest()
+                  .authenticated();
+
+    }
+}
